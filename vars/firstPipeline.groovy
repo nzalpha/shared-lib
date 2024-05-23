38d41ce14559f5f1714d98c5bebe68ed1087e686
nzalpha <148647509+nzalpha@users.noreply.github.com>
2024-05-23 10:28:56 -0700

    final

--- vars/firstPipeline.groovy
+++ vars/firstPipeline.groovy
@@ -1,27 +1,31 @@
 import com.nzdigi.builds.Calculator
-Calculator cal= new Calculator(this)
-pipeline{
-    agent any
-    environment{
-        app_name="${pipelineParams.appName}"
-    }
-    stages {
-        stage('Addition Stage')
-        {
-            steps{
-                script{
-                echo "Printing sum of 2 numbers"
-                println cal.add(19,20)
-                echo "My microservice name is: ${app_name}"
+
+def call(Map pipelineParams){
+
+    Calculator cal= new Calculator(this)
+    pipeline{
+        agent any
+        environment{
+            app_name="${pipelineParams.appName}"
+        }
+        stages {
+            stage('Addition Stage')
+            {
+                steps{
+                    script{
+                    echo "Printing sum of 2 numbers"
+                    println cal.add(19,20)
+                    echo "My microservice name is: ${app_name}"
+                    }
+                }
+            }
+
+            stage('Mailing')
+            {
+                steps{
+                    echo "Sending a mail!"
                 }
             }
         }
-
-        stage('Mailing')
-        {
-            steps{
-                echo "Sending a mail!"
-            }
-        }
     }
-}
+} 
